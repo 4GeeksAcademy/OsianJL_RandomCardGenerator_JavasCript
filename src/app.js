3085eefdec6f40e8bea1732111218098fcf3178d
Osián <149536063+OsianJL@users.noreply.github.com>
2024-01-30 16:34:52 +0000

    last update

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardNum = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const cardSym = ["♦", "♥", "♠", "♣"];
+
+  let numAleatorio = Math.floor(Math.random() * cardNum.length);
+  let cardAleatorio = Math.floor(Math.random() * cardSym.length);
+
+  let cardSimbolUp = document.getElementById("up-cardSymbol");
+  let cardSimbolDown = document.getElementById("down-cardSymbol");
+  let cardNumber = document.getElementById("cardNumber");
+
+  if (cardSym[cardAleatorio] == "♥" || cardSym[cardAleatorio] == "♦") {
+    cardSimbolDown.setAttribute("style", "color: red");
+    cardSimbolUp.setAttribute("style", "color: red");
+  }
+
+  cardNumber.innerHTML = cardNum[numAleatorio];
+  cardSimbolUp.innerHTML = cardSym[cardAleatorio];
+  cardSimbolDown.innerHTML = cardSym[cardAleatorio];
 };
